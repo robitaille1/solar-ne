d7b67fa790f27ce5f706d608ac1346bdfdcddee3
Lucas Robitaille <lrjames237@gmail.com>
2019-12-31 09:38:54 -0500

    added logo image - changed nav color

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Nav.css'
+import logo from '../../spne.png'
 
 function Nav() {
     return (
       <main className='Nav'>
         <nav className='navbar'>
             <div className='container'>
-                <h1 className='nav-link'>Solar NE</h1>
+                {/* <h1 className='nav-link'>Solar NE</h1> */}
+                <img src={logo} alt='Solar Power New England' />
             </div>
         </nav>
       </main>
