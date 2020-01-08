b90c4ba7ea14f4a072f132e3d61651b2fb3a561d
Lucas Robitaille <lrjames237@gmail.com>
2020-01-08 13:58:43 -0600

    added links and date function

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,29 @@ import React from 'react'
 import './Footer.css'
 
 function Footer() {
+  const date = new Date()
     return (
       <main className='Footer'>
         <nav className='footer'>
             <div className='container'>
-                <h5>Copyright Solar NE © 2019</h5>
+                <h5>Copyright Solar Power New England © {date.getFullYear()} </h5>
+            </div>
+            <div className='links-div'>
+              <div className='links'>
+                <p>Info</p>
+                <p>Investors</p>
+                <p>Contact</p>
+                <p>Careers</p>
+                <p>Links</p>
+              </div>
+              <div className='links'>
+                <p>Facebook</p>
+                <p>Instagram</p>
+                <p>Linkedin</p>
+              </div>
+              <div className='links'>
+                <p>Interested in helping your sustainable neighbors become more ________? Contact us today!</p>
+              </div>
             </div>
         </nav>
       </main>
