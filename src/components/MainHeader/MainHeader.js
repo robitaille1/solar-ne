fe535dcdec856149dc5196e6b7759f8da4efd6de
Lucas Robitaille <lrjames237@gmail.com>
2020-01-08 14:10:57 -0600

    added SPNE logo

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,12 +6,9 @@ function MainHeader() {
       <main className='MainHeader'>
             <div className='container'>
                 <section className='main-header' role='main'>
-                    <h2 className='main-header'>Main Header</h2>
+                    <h2 className='main-header'>Who We Are</h2>
                     <p className='main-text'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare non eros in lacinia. Proin vestibulum vulputate augue, sit amet maximus lorem fringilla ac. Integer hendrerit, urna ut pellentesque accumsan, arcu ipsum laoreet lacus, id mollis lorem nunc a ex. Donec iaculis ultrices sagittis. Vestibulum sit amet lorem non sem dignissim congue et a nunc. Etiam egestas orci ut pretium condimentum. Vestibulum rhoncus mauris vitae odio sollicitudin viverra.
-                    </p>
-                    <p>
-                        Ut vel lectus sed lacus bibendum euismod commodo sed enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent euismod enim efficitur felis maximus faucibus. In velit sapien, ornare vel neque non, viverra sagittis odio. Integer ultrices volutpat mi, sed convallis odio lacinia non. Aliquam sit amet turpis id dui auctor rhoncus lobortis sit amet odio. Curabitur posuere volutpat ornare. In neque magna, condimentum non nulla eu, eleifend viverra velit. Cras fermentum urna in varius ultricies. Maecenas interdum turpis vel ante congue, sit amet interdum orci pulvinar. Praesent vitae dui sollicitudin, euismod erat a, rhoncus quam. Morbi bibendum ultricies molestie. Praesent dignissim dolor et lectus sollicitudin sodales.
+                        Our mission is to help homeowners take ownership of their energy usage, increase their ability to manage and _______ your own power, and help them transition to the renewable future all emission. Invest in your home, community, local business and congratulations on taking your firdt step toward energy independence!
                     </p>
                 </section>
             </div>
