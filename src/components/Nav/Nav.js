a77fc688115c9fa112fe1c2e4847d968abf4c7dc
Lucas Robitaille <lucrobitaille@ymail.com>
2020-01-21 18:05:04 -0600

    added new logo - basic mobile styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import './Nav.css'
-import logo from '../../spne.png'
+import logo from '../../assets/logo.png'
 
 function Nav() {
     return (
       <main className='Nav'>
         <nav className='navbar'>
             <div className='container'>
-                {/* <h1 className='nav-link'>Solar NE</h1> */}
-                <img src={logo} alt='Solar Power New England' />
+                <img className='nav-logo' src={logo} alt='Solar Power New England' />
             </div>
         </nav>
       </main>
