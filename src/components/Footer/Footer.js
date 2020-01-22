79c39e4ef4a7c811c1ac2432c582e5c2c451a3c5
Lucas Robitaille <lucrobitaille@ymail.com>
2020-01-22 14:36:37 -0600

    added mobile first styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,23 +10,23 @@ function Footer() {
                 <h5>Copyright Solar Power New England © {date.getFullYear()} </h5>
             </div>
             <div className='links-div'>
-              <div className='links'>
-                <p>Info</p>
-                <p>Investors</p>
-                <p>Contact</p>
-                <p>Careers</p>
-                <p>Links</p>
+                <div className='links'>
+                  <p>Info</p>
+                  <p>Investors</p>
+                  <p>Contact</p>
+                  <p>Careers</p>
+                  <p>Links</p>
+                </div>
+                <div className='links'>
+                  <p>Facebook</p>
+                  <p>Instagram</p>
+                  <p>Linkedin</p>
+                </div>
+                <div className='links footer-statement'>
+                  <p>Interested in helping your neighbors become more sustainable, and energy independent? Click here for more info, or contact us today!</p>
+                  <a href="mailto:contact@solarpowerne.com" className='contact-link'>contact@solarpowerne.com</a>
+                </div>
               </div>
-              <div className='links'>
-                <p>Facebook</p>
-                <p>Instagram</p>
-                <p>Linkedin</p>
-              </div>
-              <div className='links'>
-                <p>Interested in helping your neighbors become more sustainable, and energy independent? Click here for more info, or contact us today!</p>
-                <a href="mailto:contact@solarpowerne.com">contact@solarpowerne.com</a>
-              </div>
-            </div>
             <div className='flag-div'>
               <img className='flags' src='https://www.crwflags.com/art/hist/maine1901.gif' alt='old flag of Maine'/>
               <img className='flags' src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Flag_of_Maine.svg/500px-Flag_of_Maine.svg.png' alt='current flag of Maine'/>
