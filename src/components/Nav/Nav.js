2d7db49212acc4f58c2ddee67eb1ecc29e38d3b3
Lucas Robitaille <lucrobitaille@ymail.com>
2020-01-26 14:18:40 -0600

    removed logo - added text

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Nav.css'
-import logo from '../../assets/logo.png'
+// import logo from '../../assets/logo.png'
 
 function Nav() {
     return (
       <main className='Nav'>
         <nav className='navbar container'>
-            <div>
+            {/* <div>
                 <img className='nav-logo' src={logo} alt='Solar Power New England' />
+            </div> */}
+            <div>
+              <h1 className='brand'>SOLAR POWER NEW ENGLAND</h1>
             </div>
             <div className='nav-links'>
               <a href="mailto:contact@solarpowerne.com" className='button'>Contact Us</a>
