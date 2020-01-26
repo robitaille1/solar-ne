9041273f146bf112c04bac3b6b619d430f5f4d28
Lucas Robitaille <lucrobitaille@ymail.com>
2020-01-26 14:04:27 -0600

    added blog img and alt changed background color

--- src/components/BlogSection/BlogSection.js
+++ src/components/BlogSection/BlogSection.js
@@ -9,15 +9,21 @@ class BlogSection extends Component{
       blogs: [
         {
           id: 1,
-          title: 'Plug-In Electric Vehicle Rebates'
+          title: 'Plug-In Electric Vehicle Rebates',
+          img: 'https://www.nrel.gov/news/program/2018/images/20180813-thsc-48742.jpg',
+          alt: 'electric car'
         },
         {
           id: 2,
-          title: 'Other Ways You Can Be Sustainable'
+          title: 'Other Ways You Can Be Sustainable',
+          img: 'https://theminimalistvegan.com/wp-content/uploads/2019/08/100-Simple-Tips-To-Live-a-More-Sustainable-Lifestyle-e1566537932796.jpg',
+          alt: 'person holding earth'
         },
         {
           id: 3,
-          title: 'Solar Power New England Makes Solar More Affordable'
+          title: 'Solar Power New England Makes Solar More Affordable',
+          img: 'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/EKNBCFBDA4I6RFDMSQQAMDFXXU.jpg&w=767',
+          alt: 'solar panels'
         }
       ]
     };
@@ -29,7 +35,7 @@ class BlogSection extends Component{
                 <h2 className='main-header'>Recent Blog Posts</h2>
                 <section className='blog-display' role='main'>
                    {this.state.blogs.map(blog =>
-                    <BlogItem key={blog.id} title={blog.title} />
+                    <BlogItem key={blog.id} blog={blog} />
                     )}
                 </section>
             </div>
