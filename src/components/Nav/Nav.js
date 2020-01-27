aec2eb17ac474474b80c8bbece0595f29a48d47f
Lucas Robitaille <lucrobitaille@ymail.com>
2020-01-27 11:18:44 -0600

    added black stylign to brand on mobile

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,8 +5,8 @@ function Nav() {
     return (
       <main className='Nav'>
         <nav className='navbar'>
-            <div>
-              <h1 className='brand'>SOLAR POWER NEW ENGLAND</h1>
+            <div className='brand-div'>
+              <h1 className='brand'>SOLAR POWER <span className='new-england'>NEW ENGLAND</span></h1>
             </div>
             <div className='nav-links'>
               <a href="mailto:contact@solarpowerne.com" className='button'>Contact Us</a>
