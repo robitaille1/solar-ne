0185da636debfb93908e7c440967b1358438a177
Lucas Robitaille <lucrobitaille@ymail.com>
2020-01-27 10:28:26 -0600

    removed unused code

--- src/components/WhatWeDoItem/WhatWeDoItem.js
+++ src/components/WhatWeDoItem/WhatWeDoItem.js
@@ -11,21 +11,6 @@ function WhatWeDoItem(props) {
             {props.item.description === '' ? null : <p>{props.item.description}</p>}
           </div>
         </div>
-        {/* {props.item.id % 2 === 0 ? <div className='item'>
-          <img className='itemImg' src={props.item.img} alt={props.item.credit} />
-          <div className='itemDesc'>
-            <h2 className='itemTitle'>{props.item.title}</h2>
-            {props.item.description === '' ? null : <p>{props.item.description}</p>}
-          </div>
-        </div> :
-        <div className='item'>
-          <div className='itemDesc'>
-           <h2 className='itemTitle'>{props.item.title}</h2>
-            {props.item.description === '' ? null : <p>{props.item.description}</p>}
-          </div>
-          <img className='itemImg' src={props.item.img} alt={props.item.credit} />
-        </div>
-        } */}
       </main>
     );
   }
