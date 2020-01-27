a8428c2fa50f43800a43e71d6001ae481c8f63bc
Lucas Robitaille <lucrobitaille@ymail.com>
2020-01-27 10:19:38 -0600

    removed unused code - added span for block styling

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -5,7 +5,7 @@ function Jumbotron() {
     return (
       <main className='Jumbotron container'>
         <div className='jumbo-container'>
-            <h2 className='jumbo-text'>The future is now..</h2>
+            <h2 className='jumbo-text'>The future <span className='text-2'>is now..</span></h2>
         </div>
       </main>
     );
