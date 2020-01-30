f5c093070ad319531d0b11ddf3d7ff4a3ae4ce25
Lucas Robitaille <lucrobitaille@ymail.com>
2020-01-30 17:15:50 -0600

    moved blog post state into context

---
+++ src/context/SolarContext.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default React.createContext({
+    blogs: [
+        {
+          id: '1',
+          title: 'Plug-In Electric Vehicle Rebates',
+          img: 'https://www.nrel.gov/news/program/2018/images/20180813-thsc-48742.jpg',
+          alt: 'electric car',
+          date: '01/30/2020',
+          author: 'Solar New England Team',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt auctor neque a laoreet. Nullam blandit egestas neque ac gravida. Curabitur at felis a felis dictum varius. Duis mauris odio, auctor lacinia enim non, semper iaculis ante. Vestibulum interdum vitae tellus ut molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse volutpat ante dapibus, congue nisl sit amet, dignissim nisi. Vestibulum mollis lacinia quam, quis mattis felis interdum non. Mauris in risus elementum, tempor erat et, facilisis quam. Vivamus ornare nibh et lorem interdum elementum. Nullam varius sagittis pretium. Sed ac nisi ligula. Donec suscipit eleifend elit, sit amet varius dui ullamcorper ut. Nam finibus tristique ultricies. Proin commodo quam sit amet sem facilisis viverra. Etiam non placerat erat. Pellentesque commodo, lorem sit amet blandit commodo, mauris urna viverra quam, id commodo nisl est quis sapien. Morbi elementum faucibus nulla ut imperdiet. In condimentum, ante quis dapibus consequat, arcu sapien cursus augue, vel convallis dui justo in lorem. Vestibulum convallis sed nisl vel tincidunt. Nulla auctor vehicula sapien, sed facilisis massa condimentum sed. In eu pellentesque augue, vel accumsan tellus. Quisque sit amet sapien nisi. Nullam ex turpis, sodales ut felis sed, sagittis luctus ipsum. Sed turpis risus, accumsan eget aliquam eu, ultricies id risus. Aliquam erat volutpat. Maecenas porta commodo sem et ornare. Cras porttitor ullamcorper dui. Pellentesque condimentum ipsum elementum justo tincidunt, sit amet sodales nulla dignissim. Donec sit amet felis quam. Vivamus leo sem, pharetra vel commodo ac, fermentum ut lacus. Vivamus sit amet ante scelerisque, posuere leo vel, finibus quam. Sed id magna vel nisi vulputate accumsan at sed urna. Morbi lectus ante, varius vitae felis gravida, porta rhoncus sem. Proin euismod gravida lorem ac tincidunt. Etiam leo elit, tempus fermentum augue non, faucibus egestas sapien.'
+        },
+        {
+          id: '2',
+          title: 'Other Ways You Can Be Sustainable',
+          img: 'https://theminimalistvegan.com/wp-content/uploads/2019/08/100-Simple-Tips-To-Live-a-More-Sustainable-Lifestyle-e1566537932796.jpg',
+          alt: 'person holding earth',
+          date: '01/29/2020',
+          author: 'Solar New England Team',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt auctor neque a laoreet. Nullam blandit egestas neque ac gravida. Curabitur at felis a felis dictum varius. Duis mauris odio, auctor lacinia enim non, semper iaculis ante. Vestibulum interdum vitae tellus ut molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse volutpat ante dapibus, congue nisl sit amet, dignissim nisi. Vestibulum mollis lacinia quam, quis mattis felis interdum non. Mauris in risus elementum, tempor erat et, facilisis quam. Vivamus ornare nibh et lorem interdum elementum. Nullam varius sagittis pretium. Sed ac nisi ligula. Donec suscipit eleifend elit, sit amet varius dui ullamcorper ut. Nam finibus tristique ultricies. Proin commodo quam sit amet sem facilisis viverra. Etiam non placerat erat. Pellentesque commodo, lorem sit amet blandit commodo, mauris urna viverra quam, id commodo nisl est quis sapien. Morbi elementum faucibus nulla ut imperdiet. In condimentum, ante quis dapibus consequat, arcu sapien cursus augue, vel convallis dui justo in lorem. Vestibulum convallis sed nisl vel tincidunt. Nulla auctor vehicula sapien, sed facilisis massa condimentum sed. In eu pellentesque augue, vel accumsan tellus. Quisque sit amet sapien nisi. Nullam ex turpis, sodales ut felis sed, sagittis luctus ipsum. Sed turpis risus, accumsan eget aliquam eu, ultricies id risus. Aliquam erat volutpat. Maecenas porta commodo sem et ornare. Cras porttitor ullamcorper dui. Pellentesque condimentum ipsum elementum justo tincidunt, sit amet sodales nulla dignissim. Donec sit amet felis quam. Vivamus leo sem, pharetra vel commodo ac, fermentum ut lacus. Vivamus sit amet ante scelerisque, posuere leo vel, finibus quam. Sed id magna vel nisi vulputate accumsan at sed urna. Morbi lectus ante, varius vitae felis gravida, porta rhoncus sem. Proin euismod gravida lorem ac tincidunt. Etiam leo elit, tempus fermentum augue non, faucibus egestas sapien.'
+        },
+        {
+          id: '3',
+          title: 'Solar Power New England Makes Solar More Affordable',
+          img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSeFVmiCmMT--fJ2KA-ewIE7kboA6bkWOzKS_RTrRb6WAcNFUWNg&s',
+          alt: 'solar panels',
+          date: '01/28/2020',
+          author: 'CEO',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt auctor neque a laoreet. Nullam blandit egestas neque ac gravida. Curabitur at felis a felis dictum varius. Duis mauris odio, auctor lacinia enim non, semper iaculis ante. Vestibulum interdum vitae tellus ut molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse volutpat ante dapibus, congue nisl sit amet, dignissim nisi. Vestibulum mollis lacinia quam, quis mattis felis interdum non. Mauris in risus elementum, tempor erat et, facilisis quam. Vivamus ornare nibh et lorem interdum elementum. Nullam varius sagittis pretium. Sed ac nisi ligula. Donec suscipit eleifend elit, sit amet varius dui ullamcorper ut. Nam finibus tristique ultricies. Proin commodo quam sit amet sem facilisis viverra. Etiam non placerat erat. Pellentesque commodo, lorem sit amet blandit commodo, mauris urna viverra quam, id commodo nisl est quis sapien. '
+        }
+      ]
+})
