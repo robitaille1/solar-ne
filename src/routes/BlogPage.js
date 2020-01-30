419a070fc37f7d5c494cf8447fe8037593b95e37
Lucas Robitaille <lucrobitaille@ymail.com>
2020-01-30 17:16:15 -0600

    created routes folder with route components

---
+++ src/routes/BlogPage.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { getBlog } from '../blogs-helpers'
+import SolarContext from '../context/SolarContext'
+
+import Nav from '../components/Nav/Nav'
+import BlogPost from '../components/BlogPost/BlogPost'
+
+
+
+export default class BlogPage extends React.Component {
+  static defaultProps = {
+    match: {
+      params: {}
+    }
+  }
+  
+  static contextType = SolarContext
+
+  render() {
+    const { blogs=[] } = this.context
+    const blogId = this.props.match.params.blogId
+    const blogInfo = getBlog(blogs, blogId)
+    return (
+      <main className='BlogPage'>
+        <Nav />
+        <div>
+          <BlogPost blog={blogInfo[0]} />
+        </div>
+      </main>
+    )
+  }
+}
---
+++ src/routes/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Nav from '../components/Nav/Nav'
+import Jumbotron from '../components/Jumbotron/Jumbotron'
+import MainHeader from '../components/MainHeader/MainHeader'
+import WhatWeDo from '../components/WhatWeDo/WhatWeDo'
+import ContactForm from '../components/ContactForm/ContactForm'
+import BlogSection from '../components/BlogSection/BlogSection'
+import Footer from '../components/Footer/Footer'
+
+function HomePage() {
+  return (
+    <main className='App'>
+      <Nav />
+      <Jumbotron />
+      <MainHeader />
+      <WhatWeDo />
+      <ContactForm />
+      <BlogSection />
+      <Footer />
+    </main>
+  );
+}
+
+export default HomePage
