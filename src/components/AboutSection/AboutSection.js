798752356771015d09c68867f87d3e68d54bb779
Lucas Robitaille <lucrobitaille@ymail.com>
2020-01-30 19:09:58 -0600

    created AboutSection component - added basic styling

---
+++ src/components/AboutSection/AboutSection.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import logo from '../../assets/logo.png'
+import './AboutSection.css'
+
+
+class AboutSection extends Component {
+    render() {
+        return(
+            <main className='AboutSection'>
+                <div className='about-section'>
+                    <img className='about-logo' alt='SPNE Logo' src={logo} />
+                    <h2>About Us</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed condimentum dui. Praesent luctus vitae lorem vel convallis. Etiam at pulvinar risus. Etiam fermentum erat id pellentesque pulvinar. Mauris sed blandit sapien, id sagittis justo. Cras vel euismod nisi. Donec vestibulum odio quis orci luctus, eu mattis nibh sollicitudin.
+                    </p>
+                    <p>
+                        Aenean euismod placerat elit in auctor. Fusce eget lobortis ante, quis rhoncus sapien. Mauris pellentesque malesuada purus, vitae rutrum elit. Mauris arcu mauris, egestas sed mi at, volutpat sollicitudin eros. Donec nec lacus augue. Cras vel elit cursus, gravida libero sed, blandit turpis. Mauris non lacus massa. Nunc vulputate pulvinar sollicitudin. Mauris vitae justo congue, mattis massa ac, faucibus ligula. Etiam vitae turpis maximus, ultrices neque eget, feugiat nulla.
+                    </p>
+                </div>
+            </main>
+        )
+    } 
+}
+
+export default AboutSection;
