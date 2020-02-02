d94e79f38d69dbae0d91bed72a12618170d8380a
Lucas Robitaille <lucrobitaille@ymail.com>
2020-02-02 11:18:04 -0600

    added conditional rendering for wwd items based on media width

--- src/components/WhatWeDoItem/WhatWeDoItem.js
+++ src/components/WhatWeDoItem/WhatWeDoItem.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Media from 'react-media'
 import './WhatWeDoItem.css'
 
-function WhatWeDoItem(props) {
+export default class WhatWeDoItem extends Component {
+  render(){
     return (
-      <main className='WhatWeDoItem'>
-        <div className='item'>
-          <img className='itemImg' src={props.item.img} alt={props.item.credit} />
-          <div className='itemDesc'>
-            <h2 className='itemTitle'>{props.item.title}</h2>
-            {props.item.description === '' ? null : <p>{props.item.description}</p>}
-          </div>
-        </div>
-      </main>
-    );
+      <Media queries={{ mobile: { maxWidth: 1250 } }}>
+          {matches =>
+            matches.mobile ? (
+              <main className='WhatWeDoItem'>
+                <div className='item'>
+                  <img className='itemImg' src={this.props.item.img} alt={this.props.item.credit} />
+                  <div className='itemDesc'>
+                    <h2 className='itemTitle'>{this.props.item.title}</h2>
+                    {this.props.item.description === '' ? null : <p>{this.props.item.description}</p>}
+                  </div>
+                </div>
+              </main>
+            ) : (
+              <main className='WhatWeDoItem'>
+                {this.props.item.id % 2 === 0 ? 
+                  <div className='item'>
+                    <img className='itemImg' src={this.props.item.img} alt={this.props.item.credit} />
+                    <div className='itemDesc'>
+                      <h2 className='itemTitle'>{this.props.item.title}</h2>
+                      {this.props.item.description === '' ? null : <p>{this.props.item.description}</p>}
+                    </div>
+                  </div> :
+                  <div className='item'>
+                    <div className='itemDesc'>
+                      <h2 className='itemTitle'>{this.props.item.title}</h2>
+                      {this.props.item.description === '' ? null : <p>{this.props.item.description}</p>}
+                    </div>
+                    <img className='itemImg' src={this.props.item.img} alt={this.props.item.credit} />
+                </div>
+                }
+              </main>  
+            )
+          }
+      </Media>
+    )
   }
+}
   
-export default WhatWeDoItem;
