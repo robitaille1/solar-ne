b7d97780bbf92839b80ef203f618d58c50e67e3c
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-11 14:52:42 -0500

    added routes and basic page styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import SolarContext from './context/SolarContext'
 import HomePage from './routes/HomePage'
 import AboutPage from './routes/AboutPage'
 import BlogPage from './routes/BlogPage'
+import CareersPage from './routes/CareersPage'
+import InvestorsPage from './routes/InvestorsPage'
+import PartnersPage from './routes/PartnersPage'
+import ResourcesPage from './routes/ResourcesPage'
 import './App.css'
 
 
@@ -37,6 +41,10 @@ class App extends Component {
          <main className='App'>
             <Route exact path='/' component={HomePage} />
             <Route path='/about' component={AboutPage} />
+            <Route path='/investors' component={InvestorsPage} />
+            <Route path='/partners' component={PartnersPage} />
+            <Route path='/careers' component={CareersPage} />
+            <Route path='/resources' component={ResourcesPage} />
             {this.renderBlogRoutes()}
          </main>
        </SolarContext.Provider> 
