f5105d2abe5ce8406255f1b33c7242b998a865a3
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-11 15:55:14 -0500

    added contact button and conditional rendering

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,25 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Nav.css'
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Link } from 'react-scroll';
+import './Nav.css';
 
-function Nav() {
-    return (
-      <main className='Nav'>
-        <nav className='navbar'>
-            <div className='brand-div'>
-              <Link to={`/`} style={{ textDecoration: 'none' }}>
-                <h1 className='brand'>SOLAR POWER <span className='new-england'>NEW ENGLAND</span></h1>
-              </Link>
-              
-            </div>
-            <div className='nav-links'>
-              <Link className='nav-link' to={`/about`} style={{ textDecoration: 'none' }}>
-                About Us
-              </Link>
-            </div>
-        </nav>
-      </main>
-    );
+class Nav extends Component {
+  static defaultProps = {
+    match: {
+      params: {}
+    }
   }
+
+  render() {
+    let button = 
+      <NavLink to={'/'} style={{ textDecoration: 'none' }}>
+        <button className='button'>
+          HOME
+        </button>
+      </NavLink>
+
+
+      if (this.props.path === '/') {
+        button = 
+        <Link to='contact' spy={true} smooth={true} offset={-10} duration={2000}>
+        <button className='button'>
+          CONTACT
+        </button>
+      </Link>
+      }
+      return (
+        <main className='Nav'>
+          <nav className='navbar'>
+              <div className='brand-div'>
+                <NavLink to={`/`} style={{ textDecoration: 'none' }}>
+                  <h1 className='brand'>SOLAR POWER <span className='new-england'>NEW ENGLAND</span></h1>
+                </NavLink>
+                
+              </div>
+              <div className='nav-links'>
+                <NavLink className='nav-link' to={`/about`} style={{ textDecoration: 'none' }}>
+                  ABOUT US
+                </NavLink>
+                {button}
+              </div>
+          </nav>
+        </main>
+      );
+    } 
+}
   
 export default Nav;
