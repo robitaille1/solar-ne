d31e16811a9d15d0f51b108042b785cb0f16b80b
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-11 12:28:49 -0500

    changed background image

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -7,17 +7,17 @@ function Jumbotron() {
       <main className='Jumbotron'>
         <div className='jumbo-container'>
             <h2 className='jumbo-text'>
-              Invest in your home, community, local businesses and Maine - this is your first step toward energy independence!
+              Invest in your home, community, local businesses!
               {/* The future <span className='text-2'> is now..</span> */}
             </h2>
-            {/* Photo by Scott Webb - @ScottWebb */}
+            {/* Photo by American Public Power Association - @publicpowerorg */}
             <div className='learn-more'>
               <p className='learn'>Learn more</p>
             <Link
               to='mainheader'
               spy={true}
               smooth={true}
-              offset={50}
+              offset={30}
               duration={1000}
             >
               <i className="fas fa-chevron-circle-down down-arrow"></i>
