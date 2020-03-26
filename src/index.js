6260417aee869c7af07eb99102ebf95a8d341c98
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-26 15:11:08 -0500

    added global styles deleted css file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import ScrollToTop from './utility/ScrollToTop'
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./utility/ScrollToTop";
+import App from "./App";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <ScrollToTop>
-            <App />
-        </ScrollToTop>
-    </BrowserRouter>, 
-    document.getElementById('root')
+  <BrowserRouter>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
