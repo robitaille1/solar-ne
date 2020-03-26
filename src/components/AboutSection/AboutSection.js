700684ebfffb5b283e23bb03279f07f962c42154
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-26 18:56:58 -0500

    removed css file added styled components

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import logo from '../../assets/logo.png';
-import './AboutSection.css';
+import React from "react";
+import styled from "styled-components";
+import logo from "../../assets/logo.png";
 
 const aboutSection = () => (
-    <>
-        <img className='AboutImg' alt='SPNE Logo' src={logo} />
-        <h2>About Us</h2>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed condimentum dui. Praesent luctus vitae lorem vel convallis. Etiam at pulvinar risus. Etiam fermentum erat id pellentesque pulvinar. Mauris sed blandit sapien, id sagittis justo. Cras vel euismod nisi. Donec vestibulum odio quis orci luctus, eu mattis nibh sollicitudin.
-        </p>
-        <p>
-            Aenean euismod placerat elit in auctor. Fusce eget lobortis ante, quis rhoncus sapien. Mauris pellentesque malesuada purus, vitae rutrum elit. Mauris arcu mauris, egestas sed mi at, volutpat sollicitudin eros. Donec nec lacus augue. Cras vel elit cursus, gravida libero sed, blandit turpis. Mauris non lacus massa. Nunc vulputate pulvinar sollicitudin. Mauris vitae justo congue, mattis massa ac, faucibus ligula. Etiam vitae turpis maximus, ultrices neque eget, feugiat nulla.
-        </p>
-    </>
+  <>
+    <Logo alt="SPNE Logo" src={logo} />
+    <h2>About Us</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed
+      condimentum dui. Praesent luctus vitae lorem vel convallis. Etiam at
+      pulvinar risus. Etiam fermentum erat id pellentesque pulvinar. Mauris sed
+      blandit sapien, id sagittis justo. Cras vel euismod nisi. Donec vestibulum
+      odio quis orci luctus, eu mattis nibh sollicitudin.
+    </p>
+    <p>
+      Aenean euismod placerat elit in auctor. Fusce eget lobortis ante, quis
+      rhoncus sapien. Mauris pellentesque malesuada purus, vitae rutrum elit.
+      Mauris arcu mauris, egestas sed mi at, volutpat sollicitudin eros. Donec
+      nec lacus augue. Cras vel elit cursus, gravida libero sed, blandit turpis.
+      Mauris non lacus massa. Nunc vulputate pulvinar sollicitudin. Mauris vitae
+      justo congue, mattis massa ac, faucibus ligula. Etiam vitae turpis
+      maximus, ultrices neque eget, feugiat nulla.
+    </p>
+  </>
 );
 
-
 export default aboutSection;
+
+const Logo = styled.img`
+  width: 80%;
+  @media (min-width: 700px) {
+    width: 30%;
+  }
+`;
