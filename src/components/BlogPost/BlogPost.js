f35295b55bb60ce2d48b4394408019dacb59d1ba
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-26 19:21:31 -0500

    changed formatting

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import './BlogPost.css';
+import React from "react";
+import "./BlogPost.css";
 
 const blogPost = props => (
-    <main className='BlogPost'>
-                <h2>{props.blog.title}</h2>
-                <p>By: {props.blog.author}</p>
-                <p>{props.blog.date}</p>
-                <img 
-                    className='blog-img'
-                    src={props.blog.img} 
-                    alt={props.blog.alt}
-                />
-                <div className='blog-content'>
-                    <p>{props.blog.content}</p>
-                </div>
-                {props.blog.asset === '' ? null : <img className='asset' src={props.blog.asset.table} alt='blog asset' />}
-            </main>
+  <main className="BlogPost">
+    <h2>{props.blog.title}</h2>
+    <p>By: {props.blog.author}</p>
+    <p>{props.blog.date}</p>
+    <img className="blog-img" src={props.blog.img} alt={props.blog.alt} />
+    <div className="blog-content">
+      <p>{props.blog.content}</p>
+    </div>
+    {props.blog.asset === "" ? null : (
+      <img className="asset" src={props.blog.asset.table} alt="blog asset" />
+    )}
+  </main>
 );
 
 export default blogPost;
