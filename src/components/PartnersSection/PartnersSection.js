637cb0e4af9ed24e31065f9c49858da40be18202
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-14 11:25:52 -0500

    added alt prop to images

--- src/components/PartnersSection/PartnersSection.js
+++ src/components/PartnersSection/PartnersSection.js
@@ -29,28 +29,28 @@ const partnersSection = () => (
   <>
     <h2>Our partners include...</h2>
     <Partners>
-      <img src={Canadian} />
-      <img src={Enphase} />
-      <img src={Fronius} />
-      <img src={Ironridge} />
-      <img src={Jinko} />
-      <img src={Lg} />
-      <img src={Longi} style={{ width: "100%" }} />
-      <img src={Mission} style={{ width: "100%" }} />
-      <img src={Panasonic} style={{ width: "100%" }} />
-      <img src={QCells} />
-      <img src={Rec} />
-      <img src={S5} />
-      <img src={Schneider} />
-      <img src={Silfab} />
-      <img src={Sma} />
-      <img src={Snanp} />
-      <img src={Solaredge} />
-      <img src={Solaria} />
-      <img src={Sunpower} />
-      <img src={Trina} />
-      <img src={Vikram} />
-      <img src={Yaskawa} />
+      <img alt="Canadian Solar" src={Canadian} />
+      <img alt="Enphase" src={Enphase} />
+      <img alt="Fronius" src={Fronius} />
+      <img alt="Ironridge" src={Ironridge} />
+      <img alt="Jinko" src={Jinko} />
+      <img alt="LG Electronics" src={Lg} />
+      <img alt="Longi" src={Longi} style={{ width: "100%" }} />
+      <img alt="Mission Solar Energy" src={Mission} style={{ width: "100%" }} />
+      <img alt="Panasonic" src={Panasonic} style={{ width: "100%" }} />
+      <img alt="QCells" src={QCells} />
+      <img alt="REC Group" src={Rec} />
+      <img alt="S-5" src={S5} />
+      <img alt="Schneider Electric" src={Schneider} />
+      <img alt="Silfab Solar" src={Silfab} />
+      <img alt="SMA" src={Sma} />
+      <img alt="SnanpNRack" src={Snanp} />
+      <img alt="Solar Edge" src={Solaredge} />
+      <img alt="Solaria" src={Solaria} />
+      <img alt="Sun Power" src={Sunpower} />
+      <img alt="Trina Solar" src={Trina} />
+      <img alt="Vikram Solar" src={Vikram} />
+      <img alt="Yaskawa Solectria" src={Yaskawa} />
     </Partners>
   </>
 );
@@ -62,6 +62,7 @@ const Partners = styled.div`
   grid-template-columns: 1fr;
   background: white;
   padding: 20px;
+  border-radius: 5px;
 
   @media (min-width: 600px) {
     grid-template-columns: 1fr 1fr;
@@ -79,7 +80,7 @@ const Partners = styled.div`
     grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   }
 
-  img {
+  img alt="" {
     padding: 20px;
     margin: auto;
     @media (min-width: 1000px) {
