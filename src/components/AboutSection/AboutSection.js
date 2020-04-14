8642e7d726f92f9de5386d71e455f940b26fe758
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-14 10:40:57 -0500

    added about us content

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -6,22 +6,56 @@ const aboutSection = () => (
   <>
     <Img alt="SPNE Logo" src={logo} />
     <h2>About Us</h2>
+    <p>"If I could reach the stars..."</p>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed
-      condimentum dui. Praesent luctus vitae lorem vel convallis. Etiam at
-      pulvinar risus. Etiam fermentum erat id pellentesque pulvinar. Mauris sed
-      blandit sapien, id sagittis justo. Cras vel euismod nisi. Donec vestibulum
-      odio quis orci luctus, eu mattis nibh sollicitudin.
+      From Gorham, Maine to Boston, Massachusetts...Solar Power New England
+      began as a dream as many companies do.{" "}
     </p>
     <p>
-      Aenean euismod placerat elit in auctor. Fusce eget lobortis ante, quis
-      rhoncus sapien. Mauris pellentesque malesuada purus, vitae rutrum elit.
-      Mauris arcu mauris, egestas sed mi at, volutpat sollicitudin eros. Donec
-      nec lacus augue. Cras vel elit cursus, gravida libero sed, blandit turpis.
-      Mauris non lacus massa. Nunc vulputate pulvinar sollicitudin. Mauris vitae
-      justo congue, mattis massa ac, faucibus ligula. Etiam vitae turpis
-      maximus, ultrices neque eget, feugiat nulla.
+      After having made a senior trip at the end of a college career at
+      Northeastern University... It was in Costa Rica, Andrew, the founder,
+      discovered that a renewable future was not only doable, but could be the
+      bridge to building businesses with the essence of 'pura vida' at heart,
+      and from the start. It was there that he travelled a country that is
+      today, 100% powered by renewable energy, mainly by solar, wind and
+      geothermal power.
     </p>
+    <p>
+      After careers in the IT industry, benefits administration, and financial
+      planning, it all came together and the solar industry finally came around
+      to begin hiring skillsets from multiple disciplines, environmental
+      science, engineering, and even business disciplines. Federal tax
+      incentives came into play, and the solar industry began to take off.
+    </p>
+    <p>
+      Upon return, reflection, and research, it was then that the urgency was
+      front and center. After having designed, sold and installed hundreds of
+      systems for companies since, one common theme - most systems and solar
+      installations are being done for those that have the financial means to
+      afford it. Those that can't afford it, are also commonly victims of
+      predatory billing, fees and inadequate housing.
+    </p>
+    <p>
+      In addition to the lack of diversity in customer profiles and purchasers,
+      it was also prevalent that the industry itself was woefully underdeveloped
+      in a socially-responsible manner.
+    </p>
+    <p>
+      And so, Solar Power New England was founded. SPNE's impact focus is to
+      improve the lives of its neighbors, by developing and installing solar(PV)
+      systems for those that can afford it, and promising a "profit-for-panels"
+      turnaround for families and at-risk individuals in need, free of cost to
+      their end users. Free installs, free equipment, and hopefully a little
+      hope. Giving back is not something you're supposed to do once you can
+      afford to do it... It's something we all have to do from the start.{" "}
+    </p>
+    <p>
+      Live simply so others may simply live, and give simply so others may
+      simply give.{" "}
+    </p>
+    <p>...Pull one down for you..."</p>
+    <p>Let's Change the world</p>
+    <p>-Representing the GHS class of 2005-</p>
   </>
 );
 
