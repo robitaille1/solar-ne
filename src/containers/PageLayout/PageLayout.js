d513e37b5e536485cc1362f302dc88d14ebf3f07
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-15 11:07:37 -0500

    reduced padding on mobile

--- src/containers/PageLayout/PageLayout.js
+++ src/containers/PageLayout/PageLayout.js
@@ -29,15 +29,13 @@ const Wrapper = styled.main`
 
 const Content = styled.section`
   white-space: pre-line;
-  padding: 50px 7% 0px;
+  padding: 50px 0% 0px;
   margin: 50px 5%;
   color: rgb(29, 60, 68);
   font-size: 18px;
   text-align: left;
   @media (min-width: 700px) {
-    .PageContent {
-      margin: 60px 10%;
-    }
+    padding: 50px 7% 0px;
   }
   a {
     margin: 12px auto;
