2e82e259f63a4db5f354b989335bbda1f86b73d6
lucas <lucas.robitaille@yahoo.com>
2020-07-22 18:56:13 -0500

    fixed copy

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -10,7 +10,7 @@ const aboutSection = () => (
       We're a small Maine company with a personal commitment to improving the
       lives of those in our community, and providing the best equipment at the
       best possible price to save money, energy, and our environment. If you
-      would like to learn moreabout us, contact us today at{" "}
+      would like to learn more about us, contact us today at{" "}
       <a href="mailto:contact@solarpowerne.com">contact@solarpowerne.com</a>.
     </p>
     {/* <p>
