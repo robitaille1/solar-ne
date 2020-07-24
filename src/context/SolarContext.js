74a1d84bcd1ad668a50f0175cdd9e25ff8029681
lucas <lucas.robitaille@yahoo.com>
2020-07-24 15:19:54 -0500

    updated plug in image

--- src/context/SolarContext.js
+++ src/context/SolarContext.js
@@ -1,37 +1,40 @@
-import React from 'react'
-import table from '../assets/blog-assets/EV-Table.PNG'
+import React from "react";
+import table from "../assets/blog-assets/EV-Table.PNG";
 
 export default React.createContext({
-    blogs: [
-        {
-          id: '1',
-          title: 'Plug-In Electric Vehicle Rebates',
-          img: 'https://www.nrel.gov/news/program/2018/images/20180813-thsc-48742.jpg',
-          alt: 'electric car',
-          date: '01/30/2020',
-          author: 'Solar New England Team',
-          content: `Solar Power New England has experience installing all types of Level 1 and Level 2 charging stations, as well as fast-charging commercial devices. Currently, Maine is one of the hottest markets for EV’s due to the addition of in-state rebates for Electric vehicles, and EVSE(Electric Vehicle Supply Equipment) or EV car charging equipment/wall connectors. \n \n Currently Maine offers a few incentives for electric vehicle and other equipment… \n \n For EV’s (All-Electric) -- Now, for a limited time(until December 21, 2021) there are $2,000-$7,500 rebates available for all-electric cars bought and registered in Maine, and that’s on top of Federal tax credits up to $7,500.00! \n \n For PHEV’s (Plug-in Hybrid’s) -- Don’t forget the plug-in hybrids! The Rebate for these vehicles is anywehere between $1,000.00-$2,000. \n \n Too good to be true? If you don’t believe us, refer to the table below provided by the  AFDC or Alternative Fuels Data. \n \n For a list of Eligible vehicles their estimated rebates, charge times, ranges and prices, click here.(Link: https://www.efficiencymaine.com/docs/EV-Eligibile-Vehicle-Rebates-Jan-2020.pdf). If you don’t find the vehicle you’re looking for, or think yours should be on there, give us a call! \n \n ***Claiming the credit: To claim this credit, when you do your taxes, be sure to file Form 8936(Link: https://www.irs.gov/pub/irs-pdf/f8936.pdf) along with your Form 1040 Individual Tax return(Link: https://www.irs.gov/pub/irs-pdf/i1040gi.pdf). \n \n Don’t Miss out! – If you bought a qualifying vehicle in the past 3 years, and you didn’t claim it on you tax return yet, you may still be able to get the credit by filing an amended tax return.  Please be sure to consult a qualified tax accountant for how this may affect you. If you have any other questions, contact us today!`,
-          asset: {table},
-        },
-        {
-          id: '2',
-          title: 'Other Ways You Can Be Sustainable',
-          img: 'https://theminimalistvegan.com/wp-content/uploads/2019/08/100-Simple-Tips-To-Live-a-More-Sustainable-Lifestyle-e1566537932796.jpg',
-          alt: 'person holding earth',
-          date: '01/29/2020',
-          author: 'Solar New England Team',
-          content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt auctor neque a laoreet. Nullam blandit egestas neque ac gravida. Curabitur at felis a felis dictum varius. Duis mauris odio, auctor lacinia enim non, semper iaculis ante. Vestibulum interdum vitae tellus ut molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse volutpat ante dapibus, congue nisl sit amet, dignissim nisi. Vestibulum mollis lacinia quam, quis mattis felis interdum non. Mauris in risus elementum, tempor erat et, facilisis quam. Vivamus ornare nibh et lorem interdum elementum. Nullam varius sagittis pretium. Sed ac nisi ligula. Donec suscipit eleifend elit, sit amet varius dui ullamcorper ut. Nam finibus tristique ultricies. Proin commodo quam sit amet sem facilisis viverra. \n \n Etiam non placerat erat. Pellentesque commodo, lorem sit amet blandit commodo, mauris urna viverra quam, id commodo nisl est quis sapien. Morbi elementum faucibus nulla ut imperdiet. In condimentum, ante quis dapibus consequat, arcu sapien cursus augue, vel convallis dui justo in lorem. Vestibulum convallis sed nisl vel tincidunt. Nulla auctor vehicula sapien, sed facilisis massa condimentum sed. In eu pellentesque augue, vel accumsan tellus. Quisque sit amet sapien nisi. Nullam ex turpis, sodales ut felis sed, sagittis luctus ipsum. Sed turpis risus, accumsan eget aliquam eu, ultricies id risus. Aliquam erat volutpat. Maecenas porta commodo sem et ornare. \n \n Cras porttitor ullamcorper dui. Pellentesque condimentum ipsum elementum justo tincidunt, sit amet sodales nulla dignissim. Donec sit amet felis quam. Vivamus leo sem, pharetra vel commodo ac, fermentum ut lacus. Vivamus sit amet ante scelerisque, posuere leo vel, finibus quam. Sed id magna vel nisi vulputate accumsan at sed urna. Morbi lectus ante, varius vitae felis gravida, porta rhoncus sem. Proin euismod gravida lorem ac tincidunt. Etiam leo elit, tempus fermentum augue non, faucibus egestas sapien.`,
-          asset: ''
-        },
-        {
-          id: '3',
-          title: 'Solar Power New England Makes Solar More Affordable',
-          img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSeFVmiCmMT--fJ2KA-ewIE7kboA6bkWOzKS_RTrRb6WAcNFUWNg&s',
-          alt: 'solar panels',
-          date: '01/28/2020',
-          author: 'CEO',
-          content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt auctor neque a laoreet. Nullam blandit egestas neque ac gravida. Curabitur at felis a felis dictum varius. Duis mauris odio, auctor lacinia enim non, semper iaculis ante. Vestibulum interdum vitae tellus ut molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse volutpat ante dapibus, congue nisl sit amet, dignissim nisi. Vestibulum mollis lacinia quam, quis mattis felis interdum non. Mauris in risus elementum, tempor erat et, facilisis quam. Vivamus ornare nibh et lorem interdum elementum. Nullam varius sagittis pretium. Sed ac nisi ligula. Donec suscipit eleifend elit, sit amet varius dui ullamcorper ut. Nam finibus tristique ultricies. Proin commodo quam sit amet sem facilisis viverra. \n \n Etiam non placerat erat. Pellentesque commodo, lorem sit amet blandit commodo, mauris urna viverra quam, id commodo nisl est quis sapien. Morbi elementum faucibus nulla ut imperdiet.`,
-          asset: ''
-        }
-      ]
-})
+  blogs: [
+    {
+      id: "1",
+      title: "Plug-In Electric Vehicle Rebates",
+      img:
+        "https://ca-times.brightspotcdn.com/dims4/default/3435cef/2147483647/strip/true/crop/2048x1361+0+0/resize/840x558!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F7c%2Ffd%2Fce30a395dfdbddf6106910249504%2Fla-fi-electric-vehicle-pilot-program-20160114-001",
+      alt: "electric car",
+      date: "01/30/2020",
+      author: "Solar New England Team",
+      content: `Solar Power New England has experience installing all types of Level 1 and Level 2 charging stations, as well as fast-charging commercial devices. Currently, Maine is one of the hottest markets for EV’s due to the addition of in-state rebates for Electric vehicles, and EVSE(Electric Vehicle Supply Equipment) or EV car charging equipment/wall connectors. \n \n Currently Maine offers a few incentives for electric vehicle and other equipment… \n \n For EV’s (All-Electric) -- Now, for a limited time(until December 21, 2021) there are $2,000-$7,500 rebates available for all-electric cars bought and registered in Maine, and that’s on top of Federal tax credits up to $7,500.00! \n \n For PHEV’s (Plug-in Hybrid’s) -- Don’t forget the plug-in hybrids! The Rebate for these vehicles is anywehere between $1,000.00-$2,000. \n \n Too good to be true? If you don’t believe us, refer to the table below provided by the  AFDC or Alternative Fuels Data. \n \n For a list of Eligible vehicles their estimated rebates, charge times, ranges and prices, click here.(Link: https://www.efficiencymaine.com/docs/EV-Eligibile-Vehicle-Rebates-Jan-2020.pdf). If you don’t find the vehicle you’re looking for, or think yours should be on there, give us a call! \n \n ***Claiming the credit: To claim this credit, when you do your taxes, be sure to file Form 8936(Link: https://www.irs.gov/pub/irs-pdf/f8936.pdf) along with your Form 1040 Individual Tax return(Link: https://www.irs.gov/pub/irs-pdf/i1040gi.pdf). \n \n Don’t Miss out! – If you bought a qualifying vehicle in the past 3 years, and you didn’t claim it on you tax return yet, you may still be able to get the credit by filing an amended tax return.  Please be sure to consult a qualified tax accountant for how this may affect you. If you have any other questions, contact us today!`,
+      asset: { table },
+    },
+    {
+      id: "2",
+      title: "Other Ways You Can Be Sustainable",
+      img:
+        "https://theminimalistvegan.com/wp-content/uploads/2019/08/100-Simple-Tips-To-Live-a-More-Sustainable-Lifestyle-e1566537932796.jpg",
+      alt: "person holding earth",
+      date: "01/29/2020",
+      author: "Solar New England Team",
+      content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt auctor neque a laoreet. Nullam blandit egestas neque ac gravida. Curabitur at felis a felis dictum varius. Duis mauris odio, auctor lacinia enim non, semper iaculis ante. Vestibulum interdum vitae tellus ut molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse volutpat ante dapibus, congue nisl sit amet, dignissim nisi. Vestibulum mollis lacinia quam, quis mattis felis interdum non. Mauris in risus elementum, tempor erat et, facilisis quam. Vivamus ornare nibh et lorem interdum elementum. Nullam varius sagittis pretium. Sed ac nisi ligula. Donec suscipit eleifend elit, sit amet varius dui ullamcorper ut. Nam finibus tristique ultricies. Proin commodo quam sit amet sem facilisis viverra. \n \n Etiam non placerat erat. Pellentesque commodo, lorem sit amet blandit commodo, mauris urna viverra quam, id commodo nisl est quis sapien. Morbi elementum faucibus nulla ut imperdiet. In condimentum, ante quis dapibus consequat, arcu sapien cursus augue, vel convallis dui justo in lorem. Vestibulum convallis sed nisl vel tincidunt. Nulla auctor vehicula sapien, sed facilisis massa condimentum sed. In eu pellentesque augue, vel accumsan tellus. Quisque sit amet sapien nisi. Nullam ex turpis, sodales ut felis sed, sagittis luctus ipsum. Sed turpis risus, accumsan eget aliquam eu, ultricies id risus. Aliquam erat volutpat. Maecenas porta commodo sem et ornare. \n \n Cras porttitor ullamcorper dui. Pellentesque condimentum ipsum elementum justo tincidunt, sit amet sodales nulla dignissim. Donec sit amet felis quam. Vivamus leo sem, pharetra vel commodo ac, fermentum ut lacus. Vivamus sit amet ante scelerisque, posuere leo vel, finibus quam. Sed id magna vel nisi vulputate accumsan at sed urna. Morbi lectus ante, varius vitae felis gravida, porta rhoncus sem. Proin euismod gravida lorem ac tincidunt. Etiam leo elit, tempus fermentum augue non, faucibus egestas sapien.`,
+      asset: "",
+    },
+    {
+      id: "3",
+      title: "Solar Power New England Makes Solar More Affordable",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSeFVmiCmMT--fJ2KA-ewIE7kboA6bkWOzKS_RTrRb6WAcNFUWNg&s",
+      alt: "solar panels",
+      date: "01/28/2020",
+      author: "CEO",
+      content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt auctor neque a laoreet. Nullam blandit egestas neque ac gravida. Curabitur at felis a felis dictum varius. Duis mauris odio, auctor lacinia enim non, semper iaculis ante. Vestibulum interdum vitae tellus ut molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse volutpat ante dapibus, congue nisl sit amet, dignissim nisi. Vestibulum mollis lacinia quam, quis mattis felis interdum non. Mauris in risus elementum, tempor erat et, facilisis quam. Vivamus ornare nibh et lorem interdum elementum. Nullam varius sagittis pretium. Sed ac nisi ligula. Donec suscipit eleifend elit, sit amet varius dui ullamcorper ut. Nam finibus tristique ultricies. Proin commodo quam sit amet sem facilisis viverra. \n \n Etiam non placerat erat. Pellentesque commodo, lorem sit amet blandit commodo, mauris urna viverra quam, id commodo nisl est quis sapien. Morbi elementum faucibus nulla ut imperdiet.`,
+      asset: "",
+    },
+  ],
+});
