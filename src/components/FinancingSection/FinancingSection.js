8d4983e1543683e3128040409a404d1a48620efa
lucas <lucas.robitaille@yahoo.com>
2020-08-16 17:19:19 -0500

    Created financing page with placeholder text

---
+++ src/components/FinancingSection/FinancingSection.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const financingSection = () => (
+  <>
+    <h2>Financing</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id nisl
+      vitae eros vehicula dignissim sed a ante. Aenean risus arcu, pharetra sit
+      amet lacinia id, ullamcorper vitae ipsum. Fusce accumsan sem neque, ac
+      euismod velit ullamcorper in. Vestibulum dignissim tincidunt justo vel
+      tempor. Duis facilisis ultricies ex pretium dignissim. Mauris placerat
+      congue est id dapibus. Donec dignissim eros sed tortor iaculis, vel
+      dapibus libero facilisis. Nunc odio lacus, euismod non nisi ut, imperdiet
+      condimentum lacus. Vivamus scelerisque tellus lacus, ut pharetra enim
+      eleifend vel. Maecenas auctor sed nisi et sodales. Duis a mattis diam.
+      Aenean placerat efficitur turpis ac aliquet. In aliquet sagittis ultrices.
+    </p>
+    <p>
+      Curabitur sodales facilisis placerat. Cras cursus, nunc ac semper tempor,
+      risus orci vestibulum felis, sit amet faucibus odio metus sit amet libero.
+      Donec lacus augue, tincidunt ut laoreet quis, mattis quis justo.
+      Suspendisse pellentesque lectus sit amet tellus vestibulum ornare. Vivamus
+      magna ligula, euismod sed efficitur eget, tempus auctor quam. Curabitur
+      scelerisque efficitur enim vel mollis. Nam justo augue, finibus elementum
+      placerat a, egestas sed nunc. Aliquam sit amet quam pharetra, pulvinar
+      justo sed, placerat magna.
+    </p>
+  </>
+);
+
+export default financingSection;
