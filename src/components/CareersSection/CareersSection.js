cae147a23f4eb5884091dbfa87e1c74b2cbea0ed
lucas <lucas.robitaille@yahoo.com>
2020-08-16 17:14:01 -0500

    Updated copy

--- src/components/CareersSection/CareersSection.js
+++ src/components/CareersSection/CareersSection.js
@@ -12,7 +12,8 @@ const careersSection = () => (
     </p>
     <p>
       If you're interested, please submit your resumé, contact information and a
-      brief description of yourself below! We look forward to hearing from you!
+      brief description of yourself and why you want to work for us below! We
+      look forward to hearing from you!
     </p>
     <form
       action="https://formsubmit.co/contact@solarpowerne.com"
