dfed668a668887a3dde4d2e491414654c40d3f3f
lucas <lucas.robitaille@yahoo.com>
2021-01-04 15:34:23 -0800

    Added some basic styling to images and blog content

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -1,16 +1,29 @@
 import React from "react";
 import "./BlogPost.css";
 import ReactMarkdown from "react-markdown";
+import styled from "styled-components";
 
 const blogPost = (props) => (
   <main>
     <h2>{props.blog.title}</h2>
     <p style={{ textAlign: "center" }}>By: Andy @ Solar Power NE</p>
-    <img src={props.blog.image.url} alt={props.blog.image.alternativeText} />
-    <div>
+    <img
+      style={{ maxWidth: "100%" }}
+      src={props.blog.image.url}
+      alt={props.blog.image.alternativeText}
+    />
+    <BlogContent>
       <ReactMarkdown source={props.blog.content} />
-    </div>
+    </BlogContent>
   </main>
 );
 
 export default blogPost;
+
+const BlogContent = styled.div`
+  padding-bottom: 20px;
+
+  img {
+    max-width: 100%;
+  }
+`;
