1fb5e794f3688463f02aee255071f729df28f175
lucas <lucas.robitaille@yahoo.com>
2022-01-29 10:12:15 -0800

    [chakra] added resources

--- src/components/ResourcesSection/ResourcesSection.js
+++ src/components/ResourcesSection/ResourcesSection.js
@@ -1,11 +1,22 @@
 import React from "react";
-import ReactMarkdown from "react-markdown";
+import Markdown from "../Redesign/Markdown";
+import Banner from "../Redesign/Banner";
+import { Container, Box } from "@chakra-ui/react";
 
-const resourcesSection = (props) => (
-  <div style={{ textAlign: "left", paddingBottom: "20px" }}>
-    <h2>Incentives and Resources</h2>
-    <ReactMarkdown linkTarget="_blank" source={props.content} />
-  </div>
-);
+const resourcesSection = ({ content }) => {
+  return (
+    <Box>
+      <Banner
+        title="Incentives and Resources"
+        image="url(https://res.cloudinary.com/robitaille/image/upload/v1642701443/summer_vc4x1a_xojb5o.jpg)"
+      />
+      <Container maxW={"5xl"} pb={12}>
+        <Box py={10}>
+          <Markdown content={content && content} />
+        </Box>
+      </Container>
+    </Box>
+  );
+};
 
 export default resourcesSection;
