ae1663cfa63e92bcb2e710b95097a86993a2fee9
lucas <lucas.robitaille@yahoo.com>
2022-01-31 15:24:24 -0800

    [chakra] updated partners section

--- src/components/PartnerSection/PartnerSection.js
+++ src/components/PartnerSection/PartnerSection.js
@@ -1,16 +1,50 @@
 import React from "react";
 import Markdown from "../Redesign/Markdown";
-import { Container, Box, Heading } from "@chakra-ui/react";
+import {
+  Container,
+  Box,
+  Heading,
+  Divider,
+  Link,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
+import { Link as ReachLink } from "react-router-dom";
+import { FiChevronRight } from "react-icons/fi";
 
 const partnerSection = ({ partner }) => {
   return (
     <Container maxW={"5xl"} py={12}>
       <Box textAlign={"center"}>
         <Heading>{partner && partner[0].PartnerName}</Heading>
+        <Divider mt={10} />
       </Box>
 
       <Box py={10}>
         <Markdown content={partner && partner[0].PartnerDescription} />
+        <Flex justify={"center"} mt={10}>
+          <Link as={ReachLink} to="/">
+            <Button
+              bg={"green.400"}
+              rounded={"full"}
+              color={"white"}
+              _hover={{ bg: "green.500" }}
+            >
+              Home
+            </Button>
+          </Link>
+          <Link as={ReachLink} to="/partners">
+            <Button
+              bg={"white"}
+              rounded={"full"}
+              variant={"ghost"}
+              colorScheme={"green"}
+              ml={2}
+            >
+              All Partners <FiChevronRight style={{ marginTop: "2px" }} />
+            </Button>
+          </Link>
+        </Flex>
       </Box>
     </Container>
   );
--- src/components/Redesign/PartnersCard2.js
+++ src/components/Redesign/PartnersCard2.js
@@ -9,18 +9,7 @@ import {
 } from "@chakra-ui/react";
 import { Link as ReachLink } from "react-router-dom";
 
-const data = {
-  isNew: true,
-  imageURL:
-    "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
-  name: "Wayfarer Classic",
-  price: 4.5,
-  rating: 4.2,
-  numReviews: 34,
-};
-
 function ProductAddToCart({ partner }) {
-  console.log(partner);
   return (
     <Flex p={30} w="full" alignItems="center" justifyContent="center">
       <Link
@@ -48,11 +37,22 @@ function ProductAddToCart({ partner }) {
 
           <Box p="6">
             <Box d="flex" alignItems="baseline">
-              {data.isNew && (
-                <Badge rounded="full" px="2" fontSize="0.8em" colorScheme="red">
-                  New
-                </Badge>
-              )}
+              {partner.Tags &&
+                partner.Tags.split(",").map((tag) => {
+                  return (
+                    <Badge
+                      rounded="full"
+                      px="2"
+                      fontSize="0.8em"
+                      color={"green.400"}
+                      backgroundColor={"green.50"}
+                      mr={1}
+                      key={tag}
+                    >
+                      {tag}
+                    </Badge>
+                  );
+                })}
             </Box>
             <Flex mt="1" justifyContent="space-between" alignContent="center">
               <Box
