2d290c00671f32324f2580ba9dd6b3be9cf0ddb2
lucas <lucas.robitaille@yahoo.com>
2022-02-11 10:13:52 -0800

    [chakra] updated to cloudinary image

--- src/components/Redesign/AboutModule.js
+++ src/components/Redesign/AboutModule.js
@@ -21,7 +21,7 @@ export default function SplitWithImage({ content, content2 }) {
             rounded={"md"}
             alt={"feature image"}
             src={
-              "https://images.unsplash.com/photo-1559302504-64aae6ca6b6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1337&q=80"
+              "https://res.cloudinary.com/robitaille/image/upload/v1644603181/photo-1559302504-64aae6ca6b6d_jv35de.avif"
             }
             objectFit={"cover"}
           />
