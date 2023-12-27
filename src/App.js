f29b597c8af8bc3803337f6ee29ccf8e09e3c419
Lucas Robitaille <lucas@robitaille.xyz>
2023-12-26 16:53:47 -0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { Route, Switch } from 'react-router-dom';
 import { GlobalStyles } from './global';
